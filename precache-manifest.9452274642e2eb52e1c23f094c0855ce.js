de1135a8f9a86684ff28d77e9bb1fa88ebf71db0
Rufaidashafiq <rufaidashafik@gmail.com>
2020-11-13 14:50:30 +0500

    Updates

---
+++ precache-manifest.9452274642e2eb52e1c23f094c0855ce.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1d049bf1bf54c06a329ba033c3f8a2",
+    "url": "/olxx/index.html"
+  },
+  {
+    "revision": "ab1497413cb426a0dc51",
+    "url": "/olxx/static/css/2.beaad135.chunk.css"
+  },
+  {
+    "revision": "d2ee54df152f2fc075f2",
+    "url": "/olxx/static/css/main.dad3ca43.chunk.css"
+  },
+  {
+    "revision": "ab1497413cb426a0dc51",
+    "url": "/olxx/static/js/2.aaec4338.chunk.js"
+  },
+  {
+    "revision": "968c17a0ae5f1c5fa01f92f553089218",
+    "url": "/olxx/static/js/2.aaec4338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ee54df152f2fc075f2",
+    "url": "/olxx/static/js/main.21e673ba.chunk.js"
+  },
+  {
+    "revision": "b4ce9c12dd06ccae8827",
+    "url": "/olxx/static/js/runtime-main.da19867e.js"
+  },
+  {
+    "revision": "569871f78344b1eb56611d21d680ff4b",
+    "url": "/olxx/static/media/img1.569871f7.webp"
+  },
+  {
+    "revision": "593b34f1e93d474a08a662d930bcaaa8",
+    "url": "/olxx/static/media/img2.593b34f1.webp"
+  },
+  {
+    "revision": "c9531080944dd02d3d2146b708c3bffa",
+    "url": "/olxx/static/media/img2.c9531080.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/olxx/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/olxx/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/olxx/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/olxx/static/media/slick.f97e3bbf.svg"
+  }
+]);
